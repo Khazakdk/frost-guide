9585d57c2a8988d1582d772e8b2f8f775af41824
khazakdk <joe.j.sommer@gmail.com>
2023-07-23 22:27:31 -0500

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
-  themeConfig: './theme.config.jsx'
+  themeConfig: './theme.config.jsx',
+  images: {
+    unoptimized: true
+  }
 })
  
 module.exports = withNextra()
