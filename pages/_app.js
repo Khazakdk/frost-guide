b5347eaea6ea632ceeefcaa6d56f42c8fc54d7e9
khazakdk <joe.j.sommer@gmail.com>
2023-11-07 10:39:49 -0600

    Fix WH tooltips and set up GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import './styles.css'
+import Script from 'next/script'
  
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${process.env.GA_MEASUREMENT_ID});
+        `}
+      </Script>
+      <Script id="whtooltips">
+        {`
+          const whTooltips = {colorLinks: true, iconizeLinks: true, renameLinks: true};
+        `}
+      </Script>
+      <Script src="https://wow.zamimg.com/js/tooltips.js"/>
+      <Component {...pageProps} />
+    </div>
+  )
 }
