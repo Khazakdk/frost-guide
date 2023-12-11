a000341e70f3f27ceeb0dcf74491ccba3099a29e
khazakdk <joe.j.sommer@gmail.com>
2023-12-11 10:59:22 -0600

    Manually set head tags

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,12 +1,10 @@
 export default {
   footer: <p>@khazakdk on Discord, <a href="https://twitter.com/Khazakdk" rel="author" target="_blank">@Khazakdk</a></p>,
   head: ({ title, meta }) => (
-    <>
-      {meta.description && (
-        <meta name="description" content={meta.description} />
-      )}
-      {meta.tag && <meta name="keywords" content={meta.tag} />}
-      {meta.author && <meta name="author" content={meta.author} />}
+    <>      
+      <meta name="title" content={'Frost Death Knight Guide for 10.2'} />
+      <meta name="description" content={'Frost DK guides for Obliteration and Breath of Sindragosa'} />
+      <meta name="author" content={'khazak'} />
     </>
   )
 }
